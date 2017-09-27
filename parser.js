f2a01c6ad510c0952b4c5a291a88cda300433fe9
Nandira Paturohman <nandirasp@gmail.com>
2017-09-27 15:54:29 +0700

    parsing data async Done

--- parser.js
+++ parser.js
@@ -1,25 +1,66 @@
 "use strict"
+let fs = require('fs');
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(id, fName, lName, email, phone, createdAt){
+    this.id = id
+    this.first_name = fName
+    this.last_name = lName
+    this.email = email
+    this.phone = phone
+    this.created_at = createdAt
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
+    this._newData = ''
   }
 
   get people() {
     return this._people
   }
 
-  addPerson() {}
+  addPerson(data) {
+    this._people.push(data)
+    this._newData += data.id+','+
+                    data.first_name+','+
+                    data.last_name+','+
+                    data.email+','+
+                    data.phone+','+
+                    data.created_at+'\n'
+  }
 
+  getData(callback) {
+    fs.readFile(this._file, 'utf8', function (err, data) {
+      if(err){
+        console.log('Error data file');
+      } else {
+        let people = data.split('\n')
+        for (let i = 0; i < people.length-1; i++) {
+          let dataSplit = people[i].split(',')
+          let person = new Person(dataSplit[0],dataSplit[1],dataSplit[2],dataSplit[3],dataSplit[4],new Date());
+          parser.people.push(person);
+        }
+      }
+      callback()
+    });
+  }
+
+  save() {
+    fs.appendFileSync('people.csv', this._newData, 'utf8')
+  }
 }
 
 let parser = new PersonParser('people.csv')
+parser.getData(function(){
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+  // -- add new people data --
+  // parser.addPerson(new Person(parser.people.length, 'Patur', 'Rohman', 'Patur@aawa.edu', '1-928-384-992', '2017-10-11T04:22:20-07:00'));
+  // parser.save();
+
+  console.log(`There are ${parser.people.length-1} people in the file '${parser._file}'.`)
+});
